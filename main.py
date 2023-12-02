e8873f3cbc82e027030be70d2968b5d3fc6384c1
themuhammadyor <muhammadyor.2022@gmail.com>
2023-12-02 22:46:24 +0300

    new task done

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import httpx
+import os
+from pprint import pprint
+import json
+url ='https://jsonplaceholder.typicode.com/todos'
+response = httpx.get(url=url)
+# pprint(response.json())
+os.mkdir("todos")
+data = response.json()
+print(data)
+os.chdir("todos")
+for todo in data:
+  with open(f"{todo['id']}.txt", "w") as f:
+    f.write(f"Title: {todo['title']}\n")
+    f.write(f"User Id: {todo['userId']}\n")
